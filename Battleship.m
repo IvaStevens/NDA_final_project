5186b54beb4ab44551459d0e304ab80eb0a95067
IvaStevens <istevens@andrew.cmu.edu>
2014-11-07 16:58:36 -0500

    adding code outlines for probabilty generation

--- Battleship.m
+++ Battleship.m
@@ -7,6 +7,7 @@ classdef Battleship < handle
         level
 		neuron
 		faq
+		guesses % For simplicity sake: lets keep guesses as a single number to reference a location 
     end
 	
 	methods
@@ -29,6 +30,27 @@ classdef Battleship < handle
 			while ~isempty(q)
 			%keep playing?
 			end
+		end
+		
+		function calc_probabilty_nrn(obj,guesses)
+			% This function uses the design of the neuron, the size of the board, and previous guesses
+			% to determine a matix of values that corresponds to the probability that the neuron is located
+			% at any given spot.
+		end
+		
+		function a = calc_probabilty_axn(obj,guesses)
+			% This function uses the design of the neuron, the size of the board, and previous guesses
+			% to determine a matix of values that corresponds to the probability that the neuron is located
+			% at any given spot.
+			H = hillock_locations(obj); % returns boolean matrix that refernces whether or not the axon hillock could be here
+			H(guesses) = 0;
+			uni = sum(sum(H)); % Cardinality of set for which axon could be. 
+			a = H/uni;
+		end
+		
+		function A = hillock_locations(obj)
+		% returns boolean matrix that refernces whether or not the axon hillock could be here
+		% calculated by considering size of matrix and whether
 		end
 	end
     
