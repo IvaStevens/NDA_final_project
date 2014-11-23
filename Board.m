f97afca5ceec6c7523f02b2bf341a63754a58992
IvaStevens <istevens@andrew.cmu.edu>
2014-11-23 01:22:36 -0500

    Update Board.m

--- Board.m
+++ Board.m
@@ -1,46 +1,50 @@
 classdef Board < handle
     properties
-        size
-		brd
-		axn % location of axon hillock on board
+        sze
+        board
+        axn % location of axon hillock on board
     end
-	
-	%Set up code
-	methods 
-		function obj = Board(level)
-			switch level
-				case level == 1
-					brd = zeros(7,7);
-				case level == 2
-					brd = zeros(7,9);
-				case level == 3
-					brd = zeros(9,9);		
-			end
-		end
-		
-		function getBoard()
-			return brd
-		end
-		
-		%places neuron on the board
-		function placeNeuron(obj,nrn)
-			[nLen, nWid] = size(nrn);
-			[bLen, bWid] = size(obj.brd);
-			top = randi(bLen-nLen); %logic?
-			left = randi(bWid-nWid); %logic?
-			obj.brd(top:top+nLen,left:left+nWid) = nrn;
-			% TODO: obj.axn = brd == 2. but not as x,y cordinates... just a single value...
-		end
-		
-	end
-	
-	%game play code
-	methods
-		%code tells if any part of the neuron is at this location
-		function bl = isNrn(obj,x,y)
-			bl = obj.brd(x,y) > 0;
-		end		
-	end
-	
-	
+    
+    %Set up code
+    methods
+        function obj = Board(level)
+            brd = [];
+            switch level
+                case Level.Easy
+                    brd = zeros(7,7);
+                case Level.Medium
+                    brd = zeros(7,9);
+                case Level.Hard
+                    brd = zeros(9,9);
+                otherwise
+                    brd = zeros(7,7);
+            end
+            obj.board = brd;
+        end
+        
+        % 		function brd = getBoard(obj)
+        % 			return obj.brd
+        % 		end
+        
+        %places neuron on the board
+        function placeNeuron(obj,nrn)
+            [nLen, nWid] = size(nrn);
+            [bLen, bWid] = size(obj.board);
+            top = randi(bLen-nLen); %logic?
+            left = randi(bWid-nWid); %logic?
+            obj.board(top:top+nLen-1,left:left+nWid-1) = nrn;
+            obj.axn = find(obj.board == 2);
+        end
+        
+    end
+    
+    %game play code
+    methods
+        %code tells if any part of the neuron is at this location
+        function bl = isNrn(obj,x,y)
+            bl = abs(obj.board(x,y)) > 0;
+        end
+    end
+    
+    
 end
