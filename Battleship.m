9bc8a75cfb4dd39661046b0230d8477a92996f0d
IvaStevens <istevens@andrew.cmu.edu>
2014-11-23 01:21:16 -0500

    Update Battleship.m
    cleaned up syntax. can instantiate

--- Battleship.m
+++ Battleship.m
@@ -5,53 +5,61 @@ classdef Battleship < handle
         score
         board
         level
-		neuron
-		faq
-		guesses % For simplicity sake: lets keep guesses as a single number to reference a location 
+        neuron
+        faq
+        guesses % For simplicity sake: lets keep guesses as a single number to reference a location
     end
-	
-	methods
-	%startup code
-		function  obj = Battleship()
-			obj.level = Level.Easy; %syntax?
-			obj.board = Board(level);
-			obj.neuron = Neuron(level);
-			obj.board.placeNeuron(obj.neuron);
-			obj.score = 0;
-			obj.Play
-			obj.faq = Questions();
-		end
-		
-		%This function should pull the next question.answer set
-		%from the questions class. It will repeatively call for these
-		% such that they can be displayed.
-		function Play(obj)
-			q = getNextQuestion()
-			while ~isempty(q)
-			%keep playing?
-			end
-		end
-		
-		function calc_probabilty_nrn(obj,guesses)
-			% This function uses the design of the neuron, the size of the board, and previous guesses
-			% to determine a matix of values that corresponds to the probability that the neuron is located
-			% at any given spot.
-		end
-		
-		function a = calc_probabilty_axn(obj,guesses)
-			% This function uses the design of the neuron, the size of the board, and previous guesses
-			% to determine a matix of values that corresponds to the probability that the neuron is located
-			% at any given spot.
-			H = hillock_locations(obj); % returns boolean matrix that refernces whether or not the axon hillock could be here
-			H(guesses) = 0;
-			uni = sum(sum(H)); % Cardinality of set for which axon could be. 
-			a = H/uni;
-		end
-		
-		function A = hillock_locations(obj)
-		% returns boolean matrix that refernces whether or not the axon hillock could be here
-		% calculated by considering size of matrix and whether
-		end
-	end
+    
+    methods
+        %startup code
+        function  obj = Battleship()
+            lvl = Level.Easy; %syntax?
+            obj.level = lvl;
+            obj.board = Board(lvl);
+            obj.neuron = Neuron(lvl);
+            nrn = obj.neuron.getNeuron;
+            obj.board.placeNeuron(nrn);
+            obj.score = 0;
+            %obj.Play
+            obj.faq = Questions();
+        end
+        
+        %This function should pull the next question/answer set
+        %from the questions class. It will repetively call for these
+        % such that they can be displayed.
+        function Play(obj)
+            q = obj.faq.getNextQuestion();
+            while ~isempty(q)
+                % keep playing?
+                % wait for user response
+                % send to questions class
+                % update score
+            end
+            % Say GameOver ad show final score
+        end
+        
+        function calc_probabilty_nrn(obj,guesses)
+            % This function uses the design of the neuron, the size of the board, and previous guesses
+            % to determine a matix of values that corresponds to the probability that the neuron is located
+            % at any given spot.
+        end
+        
+        function a = calc_probabilty_axn(obj,guesses)
+            % This function uses the design of the neuron, the size of the board, and previous guesses
+            % to determine a matix of values that corresponds to the probability that the neuron is located
+            % at any given spot.
+            H = hillock_locations(obj); % returns boolean matrix that refernces whether or not the axon hillock could be here
+            H(guesses) = 0;
+            uni = sum(sum(H)); % Cardinality of set for which axon could be.
+            a = H/uni;
+        end
+        
+        function A = hillock_locations(obj)
+            % returns boolean matrix that refernces whether or not the axon
+            % hillock could be here
+            % calculated by considering size of matrix and whether or not
+            % the locations guessed would barr this loctation
+        end
+    end
     
 end
