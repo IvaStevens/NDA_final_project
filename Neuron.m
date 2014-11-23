311e16ad004e8c6c0fc0435554954d08c69e6fa7
IvaStevens <istevens@andrew.cmu.edu>
2014-11-23 01:23:21 -0500

    Update Neuron.m

--- Neuron.m
+++ Neuron.m
@@ -1,31 +1,34 @@
-classdef Neuron < Board
+classdef Neuron < handle
     properties
-                
+        level
     end
-	
-	methods 
-		function obj = Neuron
-		end
-	end
-	
-	methods
-		function nrn = getNueron(level)
-			switch level
-				case level == 1
-					nrn = [ 1 0 0 0 0;...
-							1 1 2 1 1;...
-							1 0 0 0 0];
-				
-				case level == 2
-					nrn = [ 0 1 0 0 0;...
-							1 1 2 1 1;...
-							0 0 0 1 0];
-				
-				case level == 3
-					nrn = [ 0 0 0 1 0;...
-							1 1 2 1 1;...
-							0 0 1 0 0];			
-		end
-	end
-	
+    
+    methods
+        function obj = Neuron(lvl)
+            obj.level = lvl;
+        end
+    end
+    
+    methods
+        function nrn = getNeuron(obj)
+            lvl = obj.level;
+            nrn = [];
+            switch lvl
+                case Level.Easy
+                    nrn = [ 1 0 0 0 0;...
+                        1 1 2 1 1;...
+                        1 0 0 0 0];
+                case Level.Medium
+                    nrn = [ 0 1 0 0 0;...
+                        1 1 2 1 1;...
+                        0 0 0 1 0];
+                    
+                case Level.Hard
+                    nrn = [ 0 0 0 1 0;...
+                        1 1 2 1 1;...
+                        0 0 1 0 0];
+            end
+        end
+        
+    end
 end
