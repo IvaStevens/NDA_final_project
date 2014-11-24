796fdfa80dc3a82db3e04a7b6237edb0f07379bc
IvaStevens <istevens@andrew.cmu.edu>
2014-11-24 16:02:08 -0500

    fixed globals

--- projectGUI.m
+++ projectGUI.m
@@ -104,7 +104,7 @@ function edit1_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of edit1 as text
 %        str2double(get(hObject,'String')) returns contents of edit1 as a double
-
+global OB
 str1=get(hObject,'String');
 str2=str1{1};
 ansNum=str2num(str2);
